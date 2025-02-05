2fdf4b44da8dbfa1c77c381b665b19b69e85710e
PabloGMD <pablogmdsoftware@gmail.com>
2025-02-05 15:56:03 +0100

    Add validation to Bus model

--- models.py
+++ models.py
@@ -1,8 +1,18 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, model_validator
 from typing import Annotated
+from typing_extensions import Self
 
 class Bus(BaseModel):
-    bus_id: str
-    seats : int
-    seats_first_row : int
-    seats_reduced_mobility : int = 0
+    bus_id: str = Field(pattern=r'[A-Z]{2}[0-9]{2}')
+    seats : int = Field(ge=8,le=72)
+    seats_first_row : int = Field(ge=1,le=4)
+    seats_reduced_mobility : int = Field(default=0,ge=0,le=2)
+
+    @model_validator(mode='after')
+    def validate_seats_fill_all_space(self) -> Self:
+        "This function ensures that the last row of the bus is filled."
+        if (self.seats - self.seats_first_row) % 4 != 0:
+            raise ValueError(
+                "The number of seats must be a multiple of 4 after subtracting the first row."
+            )
+        return self
