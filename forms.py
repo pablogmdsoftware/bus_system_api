07e1647a7aff98968ec8be77efcc266423809938
PabloGMD <pablogmdsoftware@gmail.com>
2025-02-06 09:53:21 +0100

    Fix bus_id regex validation

--- forms.py
+++ forms.py
@@ -3,7 +3,7 @@ from typing import Annotated
 from typing_extensions import Self
 
 class BusForm(BaseModel):
-    bus_id: str = Field(pattern=r'[A-Z]{2}[0-9]{2}')
+    bus_id: str = Field(pattern=r'[A-Z]{2}[0-9]{2}',max_length=4)
     seats : int = Field(ge=8,le=72)
     seats_first_row : int = Field(ge=1,le=4)
     seats_reduced_mobility : int = Field(default=0,ge=0,le=2)
