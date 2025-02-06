7dcf8c0abf7c28aa90870726f13fd03a5cd7a8da
PabloGMD <pablogmdsoftware@gmail.com>
2025-02-06 08:41:35 +0100

    Create Bus model

---
+++ database.py
@@ -0,0 +1,18 @@
+from sqlmodel import SQLModel, Field, create_engine
+
+
+class Bus(SQLModel, table=True):
+    __tablename__ = "booking_bus"
+    bus_id: str = Field(primary_key=True)
+    seats : int = Field()
+    seats_first_row : int = Field()
+    seats_reduced_mobility : int = Field()
+
+
+engine = create_engine("postgresql://fastapi:qwer1234@localhost:5432/fastapi_test")
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+if __name__ == "__main__":
+    create_db_and_tables()
