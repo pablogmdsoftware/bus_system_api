6b7c660b943dfe348712ba3e774c59e701c330cb
PabloGMD <pablogmdsoftware@gmail.com>
2025-02-17 16:26:02 +0100

    Comment out unnecessary bus endpoints

--- main.py
+++ main.py
@@ -22,53 +22,53 @@ def get_buses(session: SessionDep, limit: int | None = None) -> list[Bus]:
         buses = session.exec(select(Bus)).all()
     return buses
 
-@app.post("/buses")
-def add_bus(form: BusForm, session: SessionDep):
-    bus = Bus(
-        bus_id = form.bus_id,
-        seats = form.seats,
-        seats_first_row = form.seats_first_row,
-        seats_reduced_mobility = form.seats_reduced_mobility,
-    )
-    try:
-        session.add(bus)
-        session.commit()
-        session.refresh(bus)
-    except IntegrityError:
-        raise HTTPException(
-            status_code=400,
-            detail=f"The bus id {form.bus_id} is already in the database",
-        )
-    return bus
-
 @app.get("/buses/{bus_id}")
-def get_bus(bus_id: str, session: SessionDep):
+def get_bus(bus_id: str, session: SessionDep) -> Bus:
     bus = session.get(Bus, bus_id)
     if not bus:
         return HTTPException(status_code=404, detail="Bus not found")
     return bus
 
-@app.put("/buses/{bus_id}")
-def update_bus(bus_id: str, form: UpdateBusForm, session: SessionDep):
-    bus = session.get(Bus, bus_id)
-    if not bus:
-        return HTTPException(status_code=404, detail="Bus not found")
-    bus.seats = form.seats
-    bus.seats_first_row  = form.seats_first_row
-    bus.seats_reduced_mobility = form.seats_reduced_mobility
-    session.add(bus)
-    session.commit()
-    session.refresh(bus)
-    return bus
+# @app.post("/buses")
+# def add_bus(form: BusForm, session: SessionDep):
+#     bus = Bus(
+#         bus_id = form.bus_id,
+#         seats = form.seats,
+#         seats_first_row = form.seats_first_row,
+#         seats_reduced_mobility = form.seats_reduced_mobility,
+#     )
+#     try:
+#         session.add(bus)
+#         session.commit()
+#         session.refresh(bus)
+#     except IntegrityError:
+#         raise HTTPException(
+#             status_code=400,
+#             detail=f"The bus id {form.bus_id} is already in the database",
+#         )
+#     return bus
 
-@app.delete("/buses/{bus_id}")
-def delete_bus(bus_id: str, session: SessionDep):
-    bus = session.get(Bus, bus_id)
-    if not bus:
-        return HTTPException(status_code=404, detail="Bus not found")
-    session.delete(bus)
-    session.commit()
-    return {"ok": True}
+# @app.put("/buses/{bus_id}")
+# def update_bus(bus_id: str, form: UpdateBusForm, session: SessionDep):
+#     bus = session.get(Bus, bus_id)
+#     if not bus:
+#         return HTTPException(status_code=404, detail="Bus not found")
+#     bus.seats = form.seats
+#     bus.seats_first_row  = form.seats_first_row
+#     bus.seats_reduced_mobility = form.seats_reduced_mobility
+#     session.add(bus)
+#     session.commit()
+#     session.refresh(bus)
+#     return bus
+
+# @app.delete("/buses/{bus_id}")
+# def delete_bus(bus_id: str, session: SessionDep):
+#     bus = session.get(Bus, bus_id)
+#     if not bus:
+#         return HTTPException(status_code=404, detail="Bus not found")
+#     session.delete(bus)
+#     session.commit()
+#     return {"ok": True}
 
 @app.get("/cities")
 def get_cities():
