fd5bd6f19247f16b758eb4a1227889cd692e9dbb
PabloGMD <pablogmdsoftware@gmail.com>
2025-02-24 20:23:40 +0100

    Improve create_engine code

--- settings.py
+++ settings.py
@@ -4,4 +4,9 @@ SECRET_KEY = "5134ab42837ae88773721ae43e200d313c5cf6a89fe4f89f85f2e5230cd3f62b"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 90
 
-engine = create_engine("postgresql://workuser:qwer1234@localhost:5432/fastapi_test")
+DB_NAME = "bus_system"
+DB_USER = "workuser"
+DB_PASSWORD = "qwer1234"
+DB_PORT = "5432"
+
+engine = create_engine(f"postgresql://{DB_USER}:{DB_PASSWORD}@localhost:{DB_PORT}/{DB_NAME}")
