07860414344dad615ccfe599443e0ccb3c2ac84b
PabloGMD <pablogmdsoftware@gmail.com>
2025-02-24 12:33:07 +0100

    Hide the secret key

--- settings.py
+++ settings.py
@@ -1,6 +1,8 @@
 from sqlmodel import create_engine
 
-SECRET_KEY = "5134ab42837ae88773721ae43e200d313c5cf6a89fe4f89f85f2e5230cd3f62b"
+with open('/etc/api_settings/secret_key.txt') as file:
+    SECRET_KEY = file.read().strip()
+
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 90
 
