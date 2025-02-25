6859da2149364924d1b561adb64d1086850c8fb3
PabloGMD <pablogmdsoftware@gmail.com>
2025-02-25 19:58:42 +0100

    Hide database constants

--- settings.py
+++ settings.py
@@ -6,9 +6,16 @@ with open('/etc/api_settings/secret_key.txt') as file:
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 90
 
-DB_NAME = "bus_system"
-DB_USER = "workuser"
-DB_PASSWORD = "qwer1234"
-DB_PORT = "5432"
+with open('/etc/api_settings/db_name.txt') as file:
+    DB_NAME = file.read().strip()
+
+with open('/etc/api_settings/db_user.txt') as file:
+    DB_USER = file.read().strip()
+
+with open('/etc/api_settings/db_password.txt') as file:
+    DB_PASSWORD = file.read().strip()
+
+with open('/etc/api_settings/db_port.txt') as file:
+    DB_PORT = file.read().strip()
 
 engine = create_engine(f"postgresql://{DB_USER}:{DB_PASSWORD}@localhost:{DB_PORT}/{DB_NAME}")
